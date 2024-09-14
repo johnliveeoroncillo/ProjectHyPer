4805d0dd06441b4067dc10fa8dc299ac5c68831b
JL <johnliveeoroncillo@gmail.com>
2024-09-14 18:29:53 +0800

    Update model.php

--- core/model.php
+++ core/model.php
@@ -17,8 +17,10 @@ class Model {
 	}
 
 	protected function initializeColumns() {
-		$result = $this->repository->query("DESC {$this->repository->parse_table($this->table)}", true);
-
+		$result = $this->repository->query("DESC {$this->repository->parse_table($this->table)}", true, true);
+		$result = array_filter($result, function($value) {
+			return !in_array($value->Field, ['created_at', 'deleted_at', 'updated_at']);
+		});
 		$this->columns = array_map(function($attr) {
 			return $attr->Field;
 		}, $result);
